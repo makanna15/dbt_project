1e324252ced2184ed448a07b70a059c45f253375
Madhuri Kanna <makanna@deloitte.com>
2025-03-21 08:38:21 +0000

    added update to macor

--- macros/get_database_name.sql
+++ macros/get_database_name.sql
@@ -1,8 +1,11 @@
 {% macro get_database_name() %}
     {%- set database_query -%}
-		select TARGET_DB_NAME from INGESTION_METADATA_RELATIONAL
-	{%- endset -%}
-	{% set result = run_query(database_query) %}
-	{% set tgt_database = result.columns[0].values()[0] %}
+		select TARGET_DATABASE_NAME from ANALYTICS.DBT_MKANNA.INGESTION_METADATA_RELATIONAL 
+    {%- endset -%}
+    {% set result = run_query(database_query) %}
+    {% if result.columns[0].values()[0] %}
+        {% set tgt_database = result.columns[0].values()[0] %}
+    {% else %} {% set tgt_database = "ANALYTICS" %}
+    {% endif %}
     {{ return(tgt_database) }}
 {% endmacro %}
