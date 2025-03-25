a509f300d23ce764bb4db0a568f7d4febe8e3eb1
Madhuri Kanna <makanna@deloitte.com>
2025-03-25 13:52:49 +0000

    bsfbs

--- models/concise_customers.sql
+++
@@ -1,5 +0,0 @@
-{{ log(var('tgt_database'), info=True) }}
-{{ config(materialized='table',database=var('tgt_database')) }}
-select id as customer_id, first_name, last_name
-
-from raw.jaffle_shop.customers
